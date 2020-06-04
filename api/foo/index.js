a92c6c10bc0f0d7cf8555af06b36d49d7d4b75f8
ashishdesai <ashishdesai21@gmail.com>
2020-06-04 12:22:11 +0530

    api trigger

--- api/foo/index.js
+++ api/foo/index.js
@@ -1,5 +1,5 @@
 const {uuid} = require('uuid')
 
 exports.handler =  async (event)  => {
-  return "This is simplest lambda for FOO :" + uuid.v4();
+  return "This is simplest lambda for FOO :-" + uuid.v4();
 }
